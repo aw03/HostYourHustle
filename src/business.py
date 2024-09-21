5a148ca61c3da1ae441f7a189486d8103b5664cc
aw03 <awu245@gmail.com>
2024-09-21 13:19:43 -0400

    finished business class

--- src/business.py
+++ src/business.py
@@ -1,11 +1,10 @@
 # A business class
-
 class Business:
     # Class variable (shared by all instances)
     __business_id = 0
 
     # Constructor (__init__) with 3 strings and a list
-    def __init__(self, name, contact, neighborhood, services : list[str], socials : str, description):
+    def __init__(self, name, contact, neighborhood, services: list[str], socials: str, description):
         # Instance variables (unique to each instance)
         self.id = Business.__new_business_id()
         self.name = name
@@ -15,22 +14,50 @@ class Business:
         self.socials = socials.split(',')
         self.description = description
 
-        # Increment the class ID each time a new object is created  
+    # Increment the class ID each time a new object is created
+    @staticmethod
     def __new_business_id():
         curr = Business.__business_id
         Business.__business_id += 1
         return curr
-    
+
+    # Getter for business ID
+    @property
+    def business_id(self):
+        return self.id
+
+    # Getter for name
+    @property
+    def get_name(self):
+        return self.name
+
+    # Getter for contact
+    @property
+    def get_contact(self):
+        return self.contact
+
+    # Getter for neighborhood
+    @property
+    def get_neighborhood(self):
+        return self.neighborhood
+
+    # Getter for services (returns the set of services)
+    @property
+    def get_services(self):
+        return self.services
+
+    # Getter for socials (returns the list of socials)
+    @property
+    def get_socials(self):
+        return self.socials
+
+    # Getter for description
+    @property
+    def get_description(self):
+        return self.description
+
     def __str__(self):
         return str(self.name)
-    
+
     def __repr__(self) -> str:
         return str(self.id)
-
-    # Method to display object details (optional)
-    def display(self):
-        print(f"Instance ID: {self.instance_id}")
-        print(f"String 1: {self.string1}")
-        print(f"String 2: {self.string2}")
-        print(f"String 3: {self.string3}")
-        print(f"List Data: {self.list_data}")
