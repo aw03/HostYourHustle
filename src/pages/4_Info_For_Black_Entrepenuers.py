a42efde648a686b92aa23e63a4d7289d927bb72d
aw03 <awu245@gmail.com>
2024-09-22 01:16:21 -0400

    Updated the Info For Entrepreneurs

--- src/pages/4_Info_For_Black_Entrepenuers.py
+++ src/pages/4_Info_For_Black_Entrepenuers.py
@@ -1,28 +1,37 @@
 import streamlit as st
 
 # Title of the app
-st.title("Scrollable Text Information")
+st.title("Find Information To Grow Your Hustle!")
 
 # Create an expander
-with st.expander("Click to see more information", expanded=False):
-    st.write("""
-    This is some detailed text information that can be scrolled down. 
-    You can include any content here, such as:
-    
-    - **Point 1**: Explanation for point 1.
-    - **Point 2**: Explanation for point 2.
-    - **Point 3**: Explanation for point 3.
-    
-    You can also add more text or even images, links, etc.
-    
-    ### Additional Information
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Quisque at semper lorem, vitae congue libero. 
-    Integer sit amet lacus neque. Cras condimentum sapien vel orci venenatis, 
-    vitae varius quam facilisis.
-    
-    You can include as much text as needed, and it will be scrollable within this section.
-    """)
+with st.expander("Beauty and Hair", expanded=False):
+    st.header("Get a Hair care liscence to start towards opening a shop!")
+    st.write(
+        """
+    A hair stylist license is your credentials or certification that demonstrate you have graduated from an approved hairdressing school or program. Hair stylists need to pass board exams, administered at the state level, to obtain their hairdresser license and practice in that state.
+    Find information here! \n   https://www.beautyschoolsdirectory.com/programs/hair-design/licensing"""
+    )
+
+with st.expander("Home Repair", expanded=False):
+    st.header("Become a Certified Renovator!")
+    st.write(
+        """
+    This certification is awarded to renovators who complete an EPA-accredited training course. The course includes 8 hours of training, including 2 hours of hands-on learning. To maintain certification, renovators must take a refresher course before their current certification expires.
+Certifications include: Certified Remodeler (CR), Certified Remodeler Specialist (CRS), Certified Remodeler Associate (CRA), and other certifications.
+             \n https://www.mass.gov/how-to/apply-for-an-individual-electrical-or-systems-license
+    """
+    )
+
+with st.expander("Gardening", expanded=False):
+    st.header("Improve your Gardening Credibility")
+    st.write(
+        """
+    National Association of Landscape Professionals (NALP) certifications
+These certifications include landscape industry certified manager, landscape industry certified technician, and landscape industry certified lawn care manager. To earn these certifications, applicants may need to pass a multiple-choice exam or a hands-on field test.
+ISA Certified Arborist
+This certification is voluntary and helps consumers identify qualified tree care professionals. \n
+https://www.landscapeprofessionals.org/LP/LP/Certification/Certification-NALP.aspx"""
+    )
 
 # Optional: Add more content outside the expander
-st.write("This is additional content outside the expander.")
+st.write("More info to come!")
